0fd429e3f7ee749b6dad86abbcb7b4f98cc4d3e1
Alessandro <Alessandro1395271@outlook.com>
2024-07-09 00:24:03 -0300

    testando comando

--- workout_api/atleta/controller.py
+++ workout_api/atleta/controller.py
@@ -140,3 +140,5 @@ async def delete(id: UUID4, db_session: DatabaseDependency) -> None:
     
     await db_session.delete(atleta)
     await db_session.commit()
+
+#testando codigos de atualizacao do gitbash
